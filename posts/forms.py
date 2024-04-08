7456c91709a3d0a7c8e3be4816790b00300fa98b
HarborUser <hareshjayaraman21@gmail.com>
2024-04-07 22:30:41 -0500

    add automatic data-time when post is added.

--- posts/forms.py
+++ posts/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Ticket
+from django.utils import timezone
 
 class TicketForm(ModelForm):  # Corrected class name
     class Meta:
         model = Ticket
-        fields = ('title', 'time_of_post', 'post_description', 'video_website_address','pdf_file', 'video_file', 'ticket_course')
-
+        
+        fields = ('title', 'post_description', 'video_website_address','pdf_file', 'video_file', 'ticket_course')
+        exclude = ['time_of_post']
+        
         labels = {
             'title': '',
             'time_of_post': '',
@@ -19,13 +22,19 @@ class TicketForm(ModelForm):  # Corrected class name
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 600px; margin: auto;', 'placeholder': 'Input your posts title'}),  # Example width: 300px
-            'time_of_post': forms.DateTimeInput(attrs={'class': 'form-control', 'style': 'width: 600px; margin: auto', 'placeholder': 'Year-Month-Day Hour:Minute:Sec'}),  # Example width: 200px
+            'time_of_post': forms.DateTimeInput(attrs={'class': 'form-control', 'style': 'width: 600px; margin: auto', 'placeholder': 'Year-Month-Day Hour:Minute:Sec','type':'datetime-local'}),  # Example width: 200px
             'post_description': forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 600px; margin: auto', 'placeholder': 'Input Post Description'}),  # Example width: 400px
             'video_website_address': forms.URLInput(attrs={'class': 'form-control', 'style': 'width: 600px; margin: auto', 'placeholder': 'Input Video URL'}),  # Example width: 200px
             'pdf_file': forms.FileInput(attrs={'class': 'form-control', 'style': 'width: 200px; margin: auto; border: none; display: inline;', 'placeholder': ''}),  # Example width: 200px
             'video_file': forms.FileInput(attrs={'class': 'form-control', 'style': 'width: 200px; margin: auto; border: none; display: inline;', 'placeholder': ''}),  # Example width: 200px
             'ticket_course': forms.Select(attrs={'class': 'form-control', 'style': 'width: 200px; margin: auto;'})  # Use Select widget for ForeignKey field
         }
+        def save(self, commit=True):
+            instance = super().save(commit=False)
+            instance.time_of_post = timezone.now()  # Set time_of_post to current date and time
+            if commit:
+                instance.save()
+            return instance
 
 
     
