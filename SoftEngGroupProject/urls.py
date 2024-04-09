2e2a1578bda5ad78ddb0d04005f5a587ab9cd474
bschaap40 <143346586+bschaap40@users.noreply.github.com>
2024-04-09 10:41:03 -0500

    Login Functionality (fix authentification)

--- SoftEngGroupProject/urls.py
+++ SoftEngGroupProject/urls.py
@@ -26,4 +26,6 @@ from django.urls import path
 urlpatterns = [
     path('', include('posts.urls')),
     path('admin/', admin.site.urls),
+    path('members/', include('members.urls')),
+    path('members/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ members/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from django.http import HttpResponse
+
+# Create your views here.
+def login_user(request):
+    if request.method =="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, "Login failed. Please try again.")
+            # Render the login page with the error message
+            return render(request, 'registration/login.html', {'message': 'Login failed. Please try again.'})
+
+
+    else:
+        return render(request, 'registration/login.html', {})
